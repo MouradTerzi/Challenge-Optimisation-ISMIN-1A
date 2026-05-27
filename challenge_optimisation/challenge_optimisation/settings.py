d15f838bb2cb2f67ac13ddf78e17ce479eea10e6
MouradTerzi <terziimourad@gmail.com>
2026-05-27 16:12:13 +0200

    add postgres support

--- challenge_optimisation/challenge_optimisation/settings.py
+++ challenge_optimisation/challenge_optimisation/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-#import dj_database_url
+import dj_database_url
 import os
 
 
@@ -90,22 +90,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'challenge_optimisation.wsgi.application'
 
-"""
+
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
     )
 }
-"""
+
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 
 # Password validation
