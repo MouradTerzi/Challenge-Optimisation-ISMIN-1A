47c69d3152f38958bb47fc72075bde0f81aac7d8
MouradTerzi <terziimourad@gmail.com>
2026-05-27 18:49:14 +0200

    feat: add c++ code of the checker

--- challenge_optimisation/build.sh
+++ challenge_optimisation/build.sh
@@ -11,4 +11,4 @@ python manage.py migrate
 python manage.py loaddata data.json
 
 # Compiler le checker directement sur Render 
-g++ -std=c++17 challenge_optimisation/checker/*.cpp -o challenge_optimisation/checker/checker 
+g++ -std=c++17 checker/*.cpp -o checker/checker 
