efd39dd92139f4235c3500abd0e1e28ed028ba93
MouradTerzi <terziimourad@gmail.com>
2026-05-28 14:31:03 +0200

    suppression des équipes de la base de données

--- challenge_optimisation/challenge_optimisation/settings.py
+++ challenge_optimisation/challenge_optimisation/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = 'django-insecure-k1nz7%a8sn7cg4ly1z*zw0zjxxe*2q0cx#t_jd6v(mfj6i8r(-
 DEBUG = False
 
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['.onrender.com']
+#ALLOWED_HOSTS = ['.onrender.com']
 
 # Application definition
 
@@ -94,25 +94,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'challenge_optimisation.wsgi.application'
 
-
+"""
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
     )
 }
-
+"""
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
